43ced58a705f928210de01eb24b6b901500306ff
James Stephaniuk <jstephaniuk@gmail.com>
2015-12-21 11:19:32 -0800

    Initial commit

---
+++ app/src/main/java/com/jstephaniuk/notifications/MainActivity.java
@@ -0,0 +1,49 @@
+package com.jstephaniuk.notifications;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    NotificationCompat.Builder notification;
+    private static final int uniqueID = 123456;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        notification = new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
+
+    }
+
+    public void sendNotificationButton(View view) {
+        //Build the notification
+        notification.setSmallIcon(R.mipmap.ic_launcher);
+        notification.setTicker("This is the ticker");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("This is the title");
+        notification.setContentText("This is the content text of the notification");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        //Builds notification and issues it
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(uniqueID, notification.build());
+    }
+
+}
